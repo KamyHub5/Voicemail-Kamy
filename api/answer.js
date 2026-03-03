eddac82fda38cdc2ce27054f30a19856af71327b
KamyHub5 <fynk@disroot.org>
2026-03-03 18:56:09 -0500

    Update answer.js

--- api/answer.js
+++ api/answer.js
@@ -17,6 +17,27 @@ module.exports = function handler(req, res) {
           number: "13059827377"
         }
       ]
+    },
+    {
+      action: "talk",
+      text: "<speak><break time='2s'/>Please leave a message at the tone. Press the pound key when finished.</speak>",
+      language: "en-GB",
+      style: 0
+    },
+    {
+      action: "record",
+      format: "mp3",
+      endOnSilence: 5,
+      endOnKey: "#",
+      timeOut: 25,
+      beepStart: true,
+      eventUrl: ["https://voicemail-kamy.vercel.app/api/recording"]
+    },
+    {
+      action: "talk",
+      text: "Your message has been recorded. Thank you. Goodbye.",
+      language: "en-GB",
+      style: 0
     }
   ]);
 }
