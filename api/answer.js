c1f9bfbda4757e3197a6ea1e1a9f91ccc9bc3d9c
KamyHub5 <fynk@disroot.org>
2026-03-03 17:58:39 -0500

    Update answer.js

--- api/answer.js
+++ api/answer.js
@@ -1,10 +1,22 @@
 module.exports = function handler(req, res) {
-  res.json([
+  res.status(200).json([
     {
       action: "talk",
-      text: "Hello. Thank you for calling. Goodbye.",
+      text: "<speak><break time='2s'/>Hello. Thank you for calling. Please remain on the line to be connected, or press the star key to leave a voicemail.</speak>",
       language: "en-GB",
       style: 0
+    },
+    {
+      action: "connect",
+      timeout: 15,
+      from: "13105151321",
+      eventUrl: ["https://voicemail-kamy.vercel.app/api/events"],
+      endpoint: [
+        {
+          type: "phone",
+          number: "13059827377"
+        }
+      ]
     }
   ]);
 }
