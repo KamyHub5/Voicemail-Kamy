566f918eb415fe252ece4c0575b7d83da2b9b1f8
KamyHub5 <fynk@disroot.org>
2026-03-03 17:26:17 -0500

    Update answer.js

--- api/answer.js
+++ api/answer.js
@@ -2,7 +2,7 @@ module.exports = function handler(req, res) {
   res.json([
     {
       action: "talk",
-      text: "Hello. Thank you for calling. Goodbye. miguel mamaguebo",
+      text: "Hello. Thank you for calling. Goodbye.",
       language: "en-GB",
       style: 0
     }
