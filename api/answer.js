879173e7b49da36170837071ab072db54ce42187
KamyHub5 <fynk@disroot.org>
2026-03-04 01:37:09 -0500

    Update answer.js

--- api/answer.js
+++ api/answer.js
@@ -1,19 +1,32 @@
-import config from "./config.js";
-
 export default function handler(req, res) {
   res.status(200).json([
     {
       action: "talk",
-      text: "<speak><prosody volume='+6dB'><break time='2s'/>Hello. Thank you for calling. Please remain on the line to be connected, or press the star key to leave a voicemail.</prosody></speak>",
-      language: "en-GB",
+      text: "Hello. To leave a voicemail, press the star key. Or stay on the line to be connected.",
+      language: "en-US",
       bargeIn: true
     },
     {
       action: "input",
       type: ["dtmf"],
-      dtmf: { timeOut: 5, maxDigits: 1 },
-      eventUrl: [`${config.BASE_URL}/api/keypress`],
+      dtmf: {
+        timeOut: 5,
+        maxDigits: 1
+      },
+      // This sends the digit you press to your keypress file
+      eventUrl: ["https://voicemail-kamy.vercel.app/api/keypress"],
       eventMethod: "POST"
+    },
+    {
+      // Fallback: If they press nothing, it just tries to connect you anyway
+      action: "connect",
+      from: "13105151321",
+      endpoint: [
+        {
+          type: "phone",
+          number: "13059827377"
+        }
+      ]
     }
   ]);
 }
