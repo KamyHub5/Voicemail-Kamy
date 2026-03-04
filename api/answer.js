d07b50e22e5c60e9f5c1466e4997271e545c51e6
KamyHub5 <fynk@disroot.org>
2026-03-03 23:30:59 -0500

    Update answer.js

--- api/answer.js
+++ api/answer.js
@@ -4,14 +4,7 @@ export default function handler(req, res) {
   res.status(200).json([
     {
       action: "talk",
-      text: `<speak>
-        <prosody volume="+6dB" pitch="low">
-          <break time="1s"/>
-          Hello. <break strength="weak"/> Thank you for calling. 
-          <prosody rate="slow">Please remain on the line to be connected,</prosody> 
-          <break strength="medium"/> or press the star key to leave a voicemail.
-        </prosody>
-      </speak>`,
+      text: "<speak><prosody volume='+6dB'><break time='2s'/>Hello. Thank you for calling. Please remain on the line to be connected, or press the star key to leave a voicemail.</prosody></speak>",
       language: "en-GB",
       bargeIn: true
     },
