5a62b4b6973cfef2ff378a1caa59ec63430bc1d7
KamyHub5 <fynk@disroot.org>
2026-03-04 15:42:41 -0500

    Update recording.js

--- api/recording.js
+++ api/recording.js
@@ -2,7 +2,7 @@ import config from "./config.js";
 
 export default async function handler(req, res) {
   try {
-    const url = `https://rest.nexmo.com/sms/json?api_key=${config.VONAGE_API_KEY}&api_secret=${config.VONAGE_API_SECRET}&from=Voicemail&to=${config.KAMY_NUMBER}&text=You+have+a+new+voicemail`;
+    const url = `https://rest.nexmo.com/sms/json?api_key=${config.VONAGE_API_KEY}&api_secret=${config.VONAGE_API_SECRET}&from=${config.VONAGE_NUMBER}&to=${config.KAMY_NUMBER}&text=You+have+a+new+voicemail`;
     const response = await fetch(url);
     const text = await response.text();
     console.log("SMS raw result:", text);
