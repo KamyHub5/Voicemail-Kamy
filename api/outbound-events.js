c44c914bba49f4e5d8dd0ce3ecdc500ddaee9cd0
KamyHub5 <fynk@disroot.org>
2026-03-03 22:51:06 -0500

    Update outbound-events.js

--- api/outbound-events.js
+++ api/outbound-events.js
@@ -3,15 +3,11 @@ const config = require("./config");
 module.exports = function handler(req, res) {
   const body = req.body;
   
-  // Log status for monitoring
-  console.log("OUTBOUND STATUS RECEIVED:", body.status);
-
-  // If the call to Kamy (13059827377) is not answered within 15s
   if (["timeout", "no-answer", "busy", "rejected", "failed"].includes(body.status)) {
     return res.status(200).json([
       {
         action: "talk",
-        text: "<speak><break time='2s'/>I'm sorry, I couldn't reach the recipient. Please leave a message after the tone. Press the pound key when finished.</speak>",
+        text: "<speak><prosody volume='x-loud'><break time='2s'/>I'm sorry, I couldn't reach the recipient. Please leave a message after the tone. Press the pound key when finished.</prosody></speak>",
         language: "en-GB"
       },
       {
@@ -24,12 +20,10 @@ module.exports = function handler(req, res) {
       },
       {
         action: "talk",
-        text: "Your message has been recorded. Thank you. Goodbye.",
+        text: "<speak><prosody volume='x-loud'>Your message has been recorded. Thank you. Goodbye.</prosody></speak>",
         language: "en-GB"
       }
     ]);
   }
-
-  // For 'started' or 'ringing' states, we return an empty 200 OK
   res.status(200).send();
 };
