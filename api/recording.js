8dcdfc63975dbe5d33fa03977c1be3f7ff381453
KamyHub5 <fynk@disroot.org>
2026-03-03 23:56:44 -0500

    Update recording.js

--- api/recording.js
+++ api/recording.js
@@ -14,34 +14,30 @@ export default async function handler(req, res) {
   
   if (body.recording_url) {
     try {
-      // 1. Get the audio from Vonage using your Private Key
-      const response = await vonage.files.get(body.recording_url);
+      const audioBuffer = await vonage.files.get(body.recording_url);
       
-      // 2. Upload to file.io as a buffer
-      const { link } = await fetch('https://file.io/?expires=1d', {
+      const fileRes = await fetch('https://file.io/?expires=1d', {
         method: 'POST',
-        body: response
-      }).then(res => res.json());
+        body: audioBuffer
+      });
+      const fileData = await fileRes.json();
+      const easyLink = fileData.link || body.recording_url;
 
-      const easyLink = link || body.recording_url;
-
-      // 3. Send the Text Message
       const dateStr = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
-      const messageText = `Voicemail Alert!
+      const textBody = `Voicemail Alert!
 From: ${body.from || 'Unknown'}
 Time: ${dateStr}
 Listen: ${easyLink}
 (Transcript to follow...)`;
 
       await vonage.sms.send({
-        to: "13059827377", 
-        from: "13105151321", 
-        text: messageText
+        to: "13059827377",
+        from: "13105151321",
+        text: textBody
       });
-      console.log("Recording link sent via SMS");
 
-    } catch (error) {
-      console.error("Recording process failed:", error);
+    } catch (err) {
+      console.error("Recording logic failed:", err);
     }
   }
   res.status(200).json({ status: "ok" });
