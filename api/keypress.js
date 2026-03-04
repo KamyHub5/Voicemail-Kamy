f8955f1a63d24c964a7e99ffab130b40f755625b
KamyHub5 <fynk@disroot.org>
2026-03-04 02:05:25 -0500

    Update keypress.js

--- api/keypress.js
+++ api/keypress.js
@@ -1,13 +1,42 @@
+import config from "./config.js";
+
 export default function handler(req, res) {
-  res.status(200).json([
-    {
-      action: "talk",
-      text: "Recording started. Hang up when done."
-    },
-    {
-      action: "record",
-      eventUrl: ["https://voicemail-kamy.vercel.app/api/recording"],
-      eventMethod: "POST"
-    }
-  ]);
+  const body = req.body || {};
+  const digit = body.dtmf?.digits || body.digits || "";
+
+  if (digit === "*") {
+    res.status(200).json([
+      {
+        action: "talk",
+        text: "<speak><prosody volume='+6dB'><break time='2s'/>Please leave a message at the tone. Press the pound key when finished.</prosody></speak>",
+        language: "en-GB"
+      },
+      {
+        action: "record",
+        format: "mp3",
+        endOnSilence: 5,
+        endOnKey: "#",
+        beepStart: true,
+        // This is the trigger for your SMS
+        eventUrl: [`${config.BASE_URL}/api/recording`],
+        eventMethod: "POST"
+      },
+      {
+        action: "talk",
+        text: "<speak><prosody volume='+6dB'>Your message has been recorded. Thank you. Goodbye.</prosody></speak>",
+        language: "en-GB"
+      }
+    ]);
+  } else {
+    res.status(200).json([
+      {
+        action: "connect",
+        timeout: 15,
+        from: config.VONAGE_NUMBER,
+        eventType: "synchronous",
+        eventUrl: [`${config.BASE_URL}/api/outbound-events`],
+        endpoint: [{ type: "phone", number: config.KAMY_NUMBER }]
+      }
+    ]);
+  }
 }
