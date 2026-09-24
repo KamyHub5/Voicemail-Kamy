9030e5506b55b59f290fe260dc4d6c030599639f
KamyHub5 <fynk@disroot.org>
2026-09-24 17:43:09 -0400

    Update inbound.js

--- api/inbound.js
+++ api/inbound.js
@@ -1,3 +1,7 @@
+// INBOUND SMS HANDLER
+// Triggered by: Incoming text messages (SMS) sent to your Vonage number
+// Purpose: Receives and processes inbound SMS webhooks from Vonage
+
 export default function handler(req, res) {
   res.status(200).json({ status: "Inbound received" });
 }
